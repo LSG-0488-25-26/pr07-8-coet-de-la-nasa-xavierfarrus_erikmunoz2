785382803fb07c94c594f3bdb1316fb429239664
Erik <erikmuar91@gmail.com>
2026-01-28 15:29:20 +0100

    Routes.kt

--- app/src/main/java/com/example/yugioh/navigation/Routes.kt
+++ app/src/main/java/com/example/yugioh/navigation/Routes.kt
@@ -1,10 +1,9 @@
 package com.example.yugioh.navigation
 
 sealed class Routes(val route: String) {
-
     object ListScreen : Routes("list")
 
-    object DetailScreen : Routes("detail/{cardIndex}") {
-        fun createRoute(cardIndex: String) = "detail/$cardIndex"
+    object DetailScreen : Routes("detail/{cardId}") {
+        fun createRoute(cardId: Int) = "detail/$cardId"
     }
 }
