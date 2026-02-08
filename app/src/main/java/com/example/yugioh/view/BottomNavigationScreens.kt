b2666357969906eb3a5f769d25d3b04bfe89de4a
Erik <erikmuar91@gmail.com>
2026-02-08 20:18:50 +0100

    Navigation screens

--- app/src/main/java/com/example/yugioh/view/BottomNavigationScreens.kt
+++ app/src/main/java/com/example/yugioh/view/BottomNavigationScreens.kt
@@ -3,11 +3,18 @@ package com.example.yugioh.view
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
- 
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.yugioh.navigation.Routes
 
 sealed class BottomNavigationScreens(val route: String, val icon: ImageVector, val label: String) {
     object Home : BottomNavigationScreens(Routes.ListScreen.route, Icons.Filled.Home, "Inicio")
     object Favorite : BottomNavigationScreens("favorite", Icons.Filled.Favorite, "Preferidos")
+
+    object Inventory : BottomNavigationScreens("inventory", Icons.Filled.Menu, "Inventario")
+
+    object Open : BottomNavigationScreens(Routes.OpenScreen.route, Icons.Filled.Star, "Apertura")
+
 }
